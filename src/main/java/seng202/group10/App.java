81c9b6667f587625762a388f3e0c263b04ddc468
Tom Rizzi <tcr59@uclive.ac.nz>
2020-07-30 11:02:26 +1200

    Implemented intSubtract method

--- src/main/java/seng202/group10/App.java
+++ src/main/java/seng202/group10/App.java
@@ -10,4 +10,15 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    /**
+     * @author Tom Rizzi
+     * @param numb1 Integer to be subtracted from
+     * @param numb2 Integer ammount to subtract form numb2
+     * @return Difference of integers
+     */
+    public int intSubtract(int numb1, int numb2) {
+        int diff = numb1 - numb2;
+        return diff;
+    }
 }
