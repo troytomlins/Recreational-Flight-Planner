94befcc0442e0f13355e029156b0a43939a75453
Mitchell <mjf153@uclive.ac.nz>
2020-08-10 20:21:35 +1200

    Added Route class

---
+++ src/main/java/seng202/group10/Route.java
@@ -0,0 +1,14 @@
+package seng202.group10;
+
+import java.util.ArrayList;
+
+public class Route {
+    private String airlineCode;
+    private int airlineId;
+    private String sourceAirportCode;
+    private Airport sourceAirport;
+    private String destinationAirportCode;
+    private Airport destinationAirport;
+    private int stops;
+    private ArrayList<String> equipment;
+}
