a88c5ce73fb168c32faef7c0a58c8f773fba0928
Mitchell <mjf153@uclive.ac.nz>
2020-08-10 20:17:13 +1200

    Added Airport class

---
+++ src/main/java/seng202/group10/Airport.java
@@ -0,0 +1,14 @@
+package seng202.group10;
+
+public class Airport {
+    private String name;
+    private String city;
+    private String iata;
+    private String icao;
+    private float latitude;
+    private float longitude;
+    private float altitude;
+    private float timezone;
+    private char dstType;
+    private String tzDatabase;
+}
