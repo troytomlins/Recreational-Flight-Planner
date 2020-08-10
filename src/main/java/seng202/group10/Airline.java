6ed3551d879afa8d3ba44e0342aba361d9382cc8
Mitchell <mjf153@uclive.ac.nz>
2020-08-10 20:24:20 +1200

    Added Airline class

---
+++ src/main/java/seng202/group10/Airline.java
@@ -0,0 +1,10 @@
+package seng202.group10;
+
+public class Airline {
+    private String name;
+    private String alias;
+    private String iata;
+    private String icao;
+    private String callsign;
+    private String country;
+}
