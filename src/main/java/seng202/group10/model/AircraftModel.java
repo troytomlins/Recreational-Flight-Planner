6ba3d543fc39aa692f98f65b9635bbaba2f636f3
Troy <tpt38@uclive.ac.nz>
2020-08-11 12:29:39 +1200

    Aircraft Model & User Model

--- src/main/java/seng202/group10/model/AircraftModel.java
+++ src/main/java/seng202/group10/model/AircraftModel.java
@@ -1,4 +1,23 @@
 package seng202.group10.model;
 
+import java.util.ArrayList;
+
 public class AircraftModel {
+    private ArrayList<Aircraft> aircraft;
+
+    /**
+     * adds new aircraft to ArrayList aircraft
+     * @param craft
+     */
+    public void addAircraft(Aircraft craft) {
+        aircraft.add(craft);
+    }
+
+    /**
+     * removes aircraft from ArrayList aircraft
+     * @param craft
+     */
+    public void deleteAircraft(Aircraft craft) {
+        aircraft.remove(craft);
+    }
 }
