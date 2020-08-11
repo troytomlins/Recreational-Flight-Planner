ac2980a3d062accf1c9eb1765c09960c5f117292
joh29 <joh29@uclive.ac.nz>
2020-08-11 13:17:49 +1200

    Hello World! Start of JavaFX app

---
+++ src/main/java/seng202/group10/view/App.java
@@ -0,0 +1,25 @@
+package seng202.group10.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/seng202/group10/view/View.java
+++ src/main/java/seng202/group10/view/View.java
@@ -1,4 +1,8 @@
 package seng202.group10.view;
 
 public class View {
+
+    public static void main(String[] args) {
+        App.main(args);
+    }
 }
