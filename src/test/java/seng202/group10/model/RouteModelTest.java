754dbbdc2a7700c887d80d9a691c1a6f8a2330e7
Zachary <zck10@uclive.ac.nz>
2020-08-18 11:22:42 +1200

    Wrote JUnit Tests for Model classes.

--- src/test/java/seng202/group10/model/RouteModelTest.java
+++ src/test/java/seng202/group10/model/RouteModelTest.java
@@ -1,12 +1,5 @@
 package seng202.group10.model;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-
-import static org.junit.Assert.assertEquals;
-
 public class RouteModelTest {
 
 }
