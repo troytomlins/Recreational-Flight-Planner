54d605f2b23997697816c8aec3acb36b2201c1d1
Troy <tpt38@uclive.ac.nz>
2020-08-18 11:14:57 +1200

    Uncommented modelFacade vals in ControllerFacade

--- src/main/java/seng202/group10/controller/ControllerFacade.java
+++ src/main/java/seng202/group10/controller/ControllerFacade.java
@@ -1,16 +1,17 @@
 package seng202.group10.controller;
 
+import seng202.group10.model.ModelFacade;
+
 public class ControllerFacade {
     private DataController dataController;
     private RawDataController rawDataController;
     private AircraftController aircraftController;
     private FlightPlanner flightPlanner;
     private MapController mapController;
+    private ModelFacade model;
 
-    // private ModelFacade = model; Not implemented
-    //
-    // public void setModel(ModelFacade model) {
-    //
-    //}
+    public void setModel(ModelFacade model) {
+
+    }
 
 }
