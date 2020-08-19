90051264713aa11a7532f8c57c2955c756a0bb46
nbt25@uclive.ac.nz <nbt25@uclive.ac.nz>
2020-08-20 07:53:25 +1200

    RWStreamTest finished

--- src/main/java/seng202/group10/model/ValidateData.java
+++ src/main/java/seng202/group10/model/ValidateData.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 
 public class ValidateData {
 
-    public static boolean Airline(ArrayList data) {
+    public static boolean Airline(ArrayList<String> data) {
         int targetSize = 8;
         return (data.size() == targetSize);
     }
 
-    public static boolean Airport(ArrayList data) {
+    public static boolean Airport(ArrayList<String> data) {
         int targetSize = 12;
         return (data.size() == targetSize);
     }
 
-    public static boolean Aircraft(ArrayList data) {
+    public static boolean Aircraft(ArrayList<String> data) {
         return true;
     }
 
-    public static boolean Route(ArrayList data) {
+    public static boolean Route(ArrayList<String> data) {
         int targetSize = 9;
         return (data.size() == targetSize);
     }
