cbd11c6f0b3258a034839f32792dba6953ca7251
Zachary <zck10@uclive.ac.nz>
2020-08-20 10:24:24 +1200

    Worked on the wrong branch.

--- src/main/java/seng202/group10/model/FlightLeg.java
+++ src/main/java/seng202/group10/model/FlightLeg.java
@@ -6,9 +6,9 @@ public class FlightLeg {
     private double latitude;
     private double longitude;
     private float altitude;
-    private float legDistance;
+    private double legDistance;
 
-    public FlightLeg(String id, String type, double latitude, double longitude, float altitude, float legDistance) {
+    public FlightLeg(String id, String type, double latitude, double longitude, float altitude, double legDistance) {
         this.id = id;
         this.type = type;
         this.latitude = latitude;
@@ -37,12 +37,20 @@ public class FlightLeg {
         return altitude;
     }
 
-    public float getLegDistance() {
+    public double getLegDistance() {
         return legDistance;
     }
 
-    public void calculateLegDistance() {
-        
+    public void setLegDistance() {
+        double latitude2 = 0;
+        double longitude2 = 0;
+        legDistance = calculateLegDistance(latitude, latitude2, longitude, longitude2);
+    }
+
+    public double calculateLegDistance(double latitude1, double latitude2, double longitude1, double longitude2) {
+        double distance = 0;
+
+        return distance;
     }
     // Distance between two points.
     // Fuel Efficency.
