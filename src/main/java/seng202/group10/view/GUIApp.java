b3f32295bdef1e0f58d97b4017316175b9b19685
Johnny Howe <jonathon.o.howe@gmail.com>
2020-08-20 14:01:54 +1200

    WebView that shows google maps (NOT API - JUST A PLACEHOLDER)

--- src/main/java/seng202/group10/view/GUIApp.java
+++ src/main/java/seng202/group10/view/GUIApp.java
@@ -1,10 +1,14 @@
 package seng202.group10.view;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import javafx.scene.web.WebEngine;
+
 import seng202.group10.controller.ControllerFacade;
 
 import java.io.IOException;
@@ -12,14 +16,22 @@ import java.io.IOException;
 
 public class GUIApp extends Application {
 
+//    @FXML
+//    WebView mapWebView;
+
     private Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Recreational Flight Planner");
-        primaryStage.setScene(new Scene(root, 1000, 650));
+        Scene scene = new Scene(root, 1000, 650);
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+        WebView mapView = (WebView) scene.lookup("#mapWebView");
+        WebEngine mapEngine = mapView.getEngine();
+        mapEngine.load("https://www.google.co.nz/maps");
     }
 
     public static void main(String[] args) {
