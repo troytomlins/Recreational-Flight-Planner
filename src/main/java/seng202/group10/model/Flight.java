5d04a77b6f33b0b99620140f067c6a3ce5880dbf
Zachary <zck10@uclive.ac.nz>
2020-08-25 15:12:51 +1200

    Wrote tests for the Flight Model class and the setLefDistances method in Flight.java.

--- src/main/java/seng202/group10/model/Flight.java
+++ src/main/java/seng202/group10/model/Flight.java
@@ -68,7 +68,7 @@ public class Flight {
     public double calculateLegDistance(double latitude1, double latitude2, double longitude1, double longitude2, double altitude1, double altitude2) {
         double distance = 0;
 
-        final int radius = 6371 * 1000; // Radius of Earth in km.
+        final int radius = 6371; // Radius of Earth in km.
 
         double latitudeDistance = Math.toRadians(latitude2 - latitude1);
         double longitudeDistance = Math.toRadians(longitude2 - longitude1);
@@ -78,11 +78,11 @@ public class Flight {
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         distance = radius * c;
 
-        double height = (altitude2 - altitude1) / 3.2808; // Height converted to meters (an approximation);
+        double height = (altitude2 - altitude1) / (3.2808 * 1000); // Height converted to km (an approximation);
 
         distance = Math.pow(distance, 2) + Math.pow(height, 2);
 
-        return (Math.sqrt(distance)) / 1000; //Convert back to kilometres.
+        return (Math.sqrt(distance)); //Convert back to kilometres.
     }
 
 }
