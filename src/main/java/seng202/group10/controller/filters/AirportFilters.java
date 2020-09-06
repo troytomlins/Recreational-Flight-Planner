d56abad324de492c79a862e09c05beb106d900fe
nbt25@uclive.ac.nz <nbt25@uclive.ac.nz>
2020-09-07 10:39:14 +1200

    filtered airports by name

---
+++ src/main/java/seng202/group10/controller/filters/AirportFilters.java
@@ -0,0 +1,31 @@
+package seng202.group10.controller.filters;
+
+import seng202.group10.model.Airline;
+import seng202.group10.model.Airport;
+import seng202.group10.model.AirportRW;
+
+import java.util.ArrayList;
+
+/**
+ * @Author Niko Tainui
+ */
+
+public class AirportFilters {
+    /**
+     * Get a new Arraylist of all the airports which contains a specific string in name.
+     * @param contains (String): substring to filter for
+     * @return ArrayList<Airline>
+     */
+
+    public ArrayList<Airport> filterByName(ArrayList<Airport> airportData, String contains) {
+        ArrayList<Airport> filteredAirports = new ArrayList<>();
+        for (Airport airport : airportData) {
+            if (airport.getName().contains(contains)) {
+                filteredAirports.add(airport);
+            }
+        }
+        return filteredAirports;
+    }
+
+
+}
