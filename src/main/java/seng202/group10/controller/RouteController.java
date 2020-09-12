6c9d7ad35b2a443f2d25fc6151f62deaf4176be7
nbt25@uclive.ac.nz <nbt25@uclive.ac.nz>
2020-09-12 15:33:40 +1200

    Made new branch

--- src/main/java/seng202/group10/controller/RouteController.java
+++ src/main/java/seng202/group10/controller/RouteController.java
@@ -1,9 +1,6 @@
 package seng202.group10.controller;
 
-import seng202.group10.model.Airport;
-import seng202.group10.model.IncompatibleFileException;
-import seng202.group10.model.Route;
-import seng202.group10.model.RouteModel;
+import seng202.group10.model.*;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -42,41 +39,10 @@ public class RouteController {
      * @param filePath Filepath string for file to import.
      */
     public void importRoutes(String filePath) throws IncompatibleFileException, IOException {
-
-        // Initialise file reader and string row variable
-        String row;
-        BufferedReader csvReader = new BufferedReader(new FileReader(filePath));
-
-        // Iterate through file reading each line
-        while ((row = csvReader.readLine()) != null) {
-
-            // Get data points from row
-            String[] data = row.split(",");
-            if (data.length != 9) {
-                throw new IncompatibleFileException();
-            }
-
-            // Get corresponding values from the value list
-            String airlineCode = data[0];
-            int airlineId = Integer.parseInt(data[1]);
-            String sourceAirportCode = data[2];
-            String sourceAirport = data[3];
-            String destinationAirportCode = data[4];
-            String destinationAirport = data[5];
-            int stops = Integer.parseInt(data[7]);
-            String equipmentString = data[8];
-
-            // Convert values to the type required for creating a route.
-            ArrayList<String> equipment = new ArrayList<String>(Arrays.asList(equipmentString.split(" ")));
-
-            // Create route and add to model
-            // Route route = new Route(airlineCode, airlineId, sourceAirportCode, sourceAirport, destinationAirportCode, destinationAirport, stops, equipment);
-            Route route = new Route(airlineCode, sourceAirportCode, destinationAirportCode, stops);
+        RouteRW stream = new RouteRW(filePath);
+        ArrayList<Route> routeList = stream.readRoutes();
+        for (Route route : routeList) {
             model.addRoute(route);
         }
-
-        // Close reader
-        csvReader.close();
     }
-
 }
