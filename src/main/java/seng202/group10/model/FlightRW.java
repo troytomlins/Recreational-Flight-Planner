5f8e3b0eff00721d2b8fec4149e2535248b29970
joh29 <joh29@uclive.ac.nz>
2020-09-15 12:12:48 +1200

    RW Stream docstrings

--- src/main/java/seng202/group10/model/FlightRW.java
+++ src/main/java/seng202/group10/model/FlightRW.java
@@ -22,40 +22,40 @@ public class FlightRW extends RWStream {
     public FlightRW() {
         super("flights.csv");
     }
-/**
-    public ArrayList<Flight> readFlights() throws IOException, IncompatibleFileException {
-        ArrayList<Flight> flights = new ArrayList<>();
-
-        // Initialise file reader and string row variable
-        BufferedReader csvReader = new BufferedReader(new FileReader(super.getInFilename()));
-
-        // Parse each line
-        CSVParser parser = CSVParser.parse(csvReader, CSVFormat.EXCEL);
-        for (CSVRecord csvRecord : parser) {
-            try {
-
-                // Get corresponding values from the csv record
-                String SrcAirportName = csvRecord.get(1);
-                String city = csvRecord.get(2);
-                String country = csvRecord.get(3);
-                String iata = csvRecord.get(4);
-                String icao = csvRecord.get(5);
-                double latitude = Double.parseDouble(csvRecord.get(6));
-                double longitude = Double.parseDouble(csvRecord.get(7));
-                float altitude = Float.parseFloat(csvRecord.get(8));
-                float timezone = Float.parseFloat(csvRecord.get(9));
-                String dstType = csvRecord.get(10);
-                String tzDatabase = csvRecord.get(11);
-
-                // Create airline and add to model
-                Airport airport = new Airport(name, city, country, iata, icao, latitude, longitude, altitude, timezone, dstType, tzDatabase);
-                airports.add(airport);
-            } catch (Exception e) {
-                throw new IncompatibleFileException();
-            }
-        }
-
-        // Close reader
-        csvReader.close();
-        return airports;
-*/  }
+//    public ArrayList<Flight> readFlights() throws IOException, IncompatibleFileException {
+//        ArrayList<Flight> flights = new ArrayList<>();
+//
+//        // Initialise file reader and string row variable
+//        BufferedReader csvReader = new BufferedReader(new FileReader(super.getInFilename()));
+//
+//        // Parse each line
+//        CSVParser parser = CSVParser.parse(csvReader, CSVFormat.EXCEL);
+//        for (CSVRecord csvRecord : parser) {
+//            try {
+//
+//                // Get corresponding values from the csv record
+//                String SrcAirportName = csvRecord.get(1);
+//                String city = csvRecord.get(2);
+//                String country = csvRecord.get(3);
+//                String iata = csvRecord.get(4);
+//                String icao = csvRecord.get(5);
+//                double latitude = Double.parseDouble(csvRecord.get(6));
+//                double longitude = Double.parseDouble(csvRecord.get(7));
+//                float altitude = Float.parseFloat(csvRecord.get(8));
+//                float timezone = Float.parseFloat(csvRecord.get(9));
+//                String dstType = csvRecord.get(10);
+//                String tzDatabase = csvRecord.get(11);
+//
+//                // Create airline and add to model
+//                Airport airport = new Airport(name, city, country, iata, icao, latitude, longitude, altitude, timezone, dstType, tzDatabase);
+//                airports.add(airport);
+//            } catch (Exception e) {
+//                throw new IncompatibleFileException();
+//            }
+//        }
+//
+//        // Close reader
+//        csvReader.close();
+//        return airports;
+//    }
+}
