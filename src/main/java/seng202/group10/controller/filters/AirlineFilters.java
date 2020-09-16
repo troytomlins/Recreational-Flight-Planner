046b61f8cf78dd77f6223b07de053bfa92eeb080
joh29 <joh29@uclive.ac.nz>
2020-09-16 21:11:16 +1200

    AirlineFilters reworking and docs

--- src/main/java/seng202/group10/controller/filters/AirlineFilters.java
+++ src/main/java/seng202/group10/controller/filters/AirlineFilters.java
@@ -18,70 +18,47 @@ public class AirlineFilters {
      * @return ArrayList<Airline>
      */
     public ArrayList<Airline> filterByName(ArrayList<Airline> airlineData, String contains) {
-        ArrayList<Airline> filteredAirlines = new ArrayList<>();
-        if (contains == "") {
-            filteredAirlines = airlineData;
-        } else {
-            for (Airline airline : airlineData) {
-                if (airline.getName().contains(contains)) {
-                    filteredAirlines.add(airline);
-                }
-            }
-        }
-        return filteredAirlines;
+        return filterByAll(airlineData, contains, "", "");
     }
 
+    /**
+     * Get a new Arraylist of all the airlines with contains string in alias.
+     * @param contains (String): substring to filter for
+     * @return ArrayList<Airline>
+     */
     public ArrayList<Airline> filterByAlias(ArrayList<Airline> airlineData, String contains) {
-        ArrayList<Airline> filteredAirlines = new ArrayList<>();
-        if (contains == "") {
-            filteredAirlines = airlineData;
-        } else {
-            for (Airline airline : airlineData) {
-                if (airline.getAlias().contains(contains)) {
-                    filteredAirlines.add(airline);
-                }
-            }
-        }
-        return filteredAirlines;
+        return filterByAll(airlineData, "", contains, "");
+
     }
 
+    /**
+     * Get a new Arraylist of all the airlines with contains string in country.
+     * @param contains (String): substring to filter for
+     * @return ArrayList<Airline>
+     */
     public ArrayList<Airline> filterByCountry(ArrayList<Airline> airlineData, String contains) {
+        return filterByAll(airlineData, "", "", contains);
+    }
+
+    /**
+     * Get a new Arraylist of all the airlines
+     * Super filter method - no point repeating the same code in multiple methods
+     * @param nameContains (String): substring to filter for in the airline names
+     * @param aliasContains (String): substring to filter for in the airline aliases
+     * @param countryContains (String): substring to filter for in the airline countries
+     * @return ArrayList<Airline>
+     */
+    public ArrayList<Airline> filterByAll(ArrayList<Airline> airlineData, String nameContains, String aliasContains, String countryContains) {
         ArrayList<Airline> filteredAirlines = new ArrayList<>();
-        if (contains == "") {
-            filteredAirlines = airlineData;
-        } else {
+        // Does at least one filter exist?
+        if (!(nameContains.equals("") && aliasContains.equals("") && countryContains.equals(""))) {
             for (Airline airline : airlineData) {
-                if (airline.getCountry().contains(contains)) {
+                // Does the airline pass the filters?
+                if (airline.getName().contains(nameContains) && airline.getAlias().contains(aliasContains) && airline.getCountry().contains(countryContains)) {
                     filteredAirlines.add(airline);
                 }
             }
         }
         return filteredAirlines;
     }
-
-    public ArrayList<Airline> filterByAll(ArrayList<Airline> airlineData, String name, String alias, String country) {
-        ArrayList<Airline> airlinesByName = new ArrayList<>();
-        ArrayList<Airline> airlinesByNameAlias = new ArrayList<>();
-        ArrayList<Airline> airlinesByNameAliasCountry = new ArrayList<>();
-        airlinesByName = filterByName(airlineData, name);
-        airlinesByNameAlias = filterByAlias(airlinesByName, alias);
-        airlinesByNameAliasCountry = filterByCountry(airlinesByNameAlias, country);
-        return airlinesByNameAliasCountry;
-    }
-
-
-
-
-
-//    public static void main(String[] args) {
-//        AirlineRW rw = new AirlineRW();
-//        ArrayList<Airline> airlines = rw.readAirlines();
-//
-//        AirlineFilters airlineFilters = new AirlineFilters();
-//
-//        ArrayList<Airline> filtered = airlineFilters.filterByName(airlines, "Blue");
-//        for (Airline airline : filtered) {
-//            System.out.println(airline);
-//        }
-//    }
 }
