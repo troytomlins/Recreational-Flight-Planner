59882fb9532f2caf74185132f0ab11646d1f4f89
joh29 <joh29@uclive.ac.nz>
2020-09-16 20:56:45 +1200

    tag typo in View fixed

--- src/main/java/seng202/group10/view/View.java
+++ src/main/java/seng202/group10/view/View.java
@@ -4,7 +4,7 @@ package seng202.group10.view;
  * Runs the app!
  * Only purpose is to call GUIApp.main
  * Seems useless on the surface but is required for the app to run
- * @Author Tom Rizzi
+ * @author Tom Rizzi
  */
 public class View {
     public static void main(String[] args) {
