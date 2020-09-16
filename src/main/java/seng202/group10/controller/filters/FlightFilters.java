3a73de477b22a7fb16d97b4a323508ea6c71ce43
joh29 <joh29@uclive.ac.nz>
2020-09-16 21:42:19 +1200

    Commented out body of FlightFilters. What should it contain? (Was just copy and pasted from AirlineFilters)

--- src/main/java/seng202/group10/controller/filters/FlightFilters.java
+++ src/main/java/seng202/group10/controller/filters/FlightFilters.java
@@ -1,76 +1,73 @@
 package seng202.group10.controller.filters;
 
-import seng202.group10.model.Airport;
-import seng202.group10.model.AirportRW;
-import seng202.group10.model.DatabaseConnection;
+import seng202.group10.model.*;
 
 import java.util.ArrayList;
 
 //TODO write the filters depending on GUI
 /**
+ * Class to filter the airline data.
  * @Author Niko Tainui
  * @Author Johnny Howe
  */
 
+// TODO what should it filter?
 public class FlightFilters {
-    /**
-     * Get a new Arraylist of all the points which contains a specific string in name.
-     * @param contains (String): substring to filter for
-     * @return ArrayList<Points>
-     */
-
-    DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
-
-    public ArrayList<Airport> filterByName(ArrayList<Airport> airportData, String contains) {
-        ArrayList<Airport> filteredAirports = new ArrayList<>();
-        if (contains == "") {
-            filteredAirports = airportData;
-        } else {
-            for (Airport airport : airportData) {
-                if (airport.getName().contains(contains)) {
-                    filteredAirports.add(airport);
-                }
-            }
-        }
-        return filteredAirports;
-    }
-
-    public ArrayList<Airport> filterByCity(ArrayList<Airport> airportData, String contains) {
-        ArrayList<Airport> filteredAirports = new ArrayList<>();
-        if (contains == "") {
-            filteredAirports = airportData;
-        } else {
-            for (Airport airport : airportData) {
-                if (airport.getCity().contains(contains)) {
-                    filteredAirports.add(airport);
-                }
-            }
-        }
-        return filteredAirports;
-    }
-
-    public ArrayList<Airport> filterByCountry(ArrayList<Airport> airportData, String contains) {
-        ArrayList<Airport> filteredAirports = new ArrayList<>();
-        if (contains == "") {
-            filteredAirports = airportData;
-        }
-        for (Airport airport : airportData) {
-            if (airport.getCountry().contains(contains)) {
-                filteredAirports.add(airport);
-            }
-        }
-        return filteredAirports;
-    }
-
-    //TODO write error checking for filters making sure data is loaded.
-
-    public ArrayList<Airport> filterByAll(ArrayList<Airport> airportData, String name, String city, String country) {
-        ArrayList<Airport> airportsByName = new ArrayList<>();
-        ArrayList<Airport> airportsByNameCity = new ArrayList<>();
-        ArrayList<Airport> airportsByNameCityCountry = new ArrayList<>();
-        airportsByName = filterByName(airportData, name);
-        airportsByNameCity = filterByCity(airportsByName, city);
-        airportsByNameCityCountry = filterByCountry(airportsByNameCity, country);
-        return airportsByNameCityCountry;
-    }
+//
+//    // TODO what does this do - put in comment pls
+//    DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+//
+//    /**
+//     * Get a new Arraylist of all the airlines with contains string in name.
+//     * @param flightData data to filter through
+//     * @param contains substring to filter for
+//     * @return the airline data filtered by names containing contains
+//     */
+//    public ArrayList<Airline> filterByName(ArrayList<Flight> flightData, String contains) {
+//        return filterByAll(flightData, contains, "", "");
+//    }
+//
+//    /**
+//     * Get a new Arraylist of all the airlines with contains string in alias.
+//     * @param flightData data to filter through
+//     * @param contains substring to filter for
+//     * @return the airline data filtered by alias containing contains
+//     */
+//    public ArrayList<Airline> filterByAlias(ArrayList<Flight> flightData, String contains) {
+//        return filterByAll(flightData, "", contains, "");
+//
+//    }
+//
+//    /**
+//     * Get a new Arraylist of all the airlines with contains string in country.
+//     * @param flightData data to filter through
+//     * @param contains substring to filter for
+//     * @return the airline data filtered by countries containing contains
+//     */
+//    public ArrayList<Airline> filterByCountry(ArrayList<Flight> flightData, String contains) {
+//        return filterByAll(flightData, "", "", contains);
+//    }
+//
+//    /**
+//     * Get a new Arraylist of all the airlines
+//     * Super filter method - no point repeating the same code in multiple methods
+//     * @param flightData data to filter through
+//     * @param nameContains substring to filter for in the airline names
+//     * @param aliasContains substring to filter for in the airline aliases
+//     * @param countryContains substring to filter for in the airline countries
+//     * @return the airline data filtered by nameContains, aliasContains and countryContains
+//     */
+//    public ArrayList<Airline> filterByAll(ArrayList<Flight> flightData, String nameContains, String aliasContains, String countryContains) {
+//        ArrayList<Airline> filteredAirlines = new ArrayList<>();
+//        // Does at least one filter exist?
+//        if (!(nameContains.equals("") && aliasContains.equals("") && countryContains.equals(""))) {
+//            for (Flight flight : flightData) {
+//                // Does the airline pass the filters?
+//                if (flight.getName().contains(nameContains) && flight.getAlias().contains(aliasContains) && flight.getCountry().contains(countryContains)) {
+//                    filteredAirlines.add(flight);
+//                }
+//            }
+//        }
+//        return filteredAirlines;
+//    }
 }
