b5be878b1430058239956e3744007a80d71b1bf8
nbt25@uclive.ac.nz <nbt25@uclive.ac.nz>
2020-09-17 12:50:14 +1200

    tests made for the flight model and flight classes also added dependancy to test private variables

--- src/test/java/seng202/group10/model/FlightModelTest.java
+++ src/test/java/seng202/group10/model/FlightModelTest.java
@@ -8,5 +8,29 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class FlightModelTest {
+    private FlightModel flightModel;
+    private Flight flight;
+    private ArrayList<Flight> compareFlight;
 
+    @BeforeEach
+    public void init() {
+        flightModel = new FlightModel();
+        flight = new Flight();
+        compareFlight = new ArrayList<>();
+        compareFlight.add(flight);
+
+    }
+
+    @Test
+    public void addFlightTest(){
+        flightModel.addFlight(flight);
+        assertEquals(compareFlight, flightModel.getFlights());
+    }
+
+    @Test
+    public void addDuplicateFlightTest() {
+        flightModel.addFlight(flight);
+        flightModel.addFlight(flight); // Try and add the same flight again.
+        assertEquals(compareFlight, flightModel.getFlights()); // The duplicate should not be added.
+    }
 }
