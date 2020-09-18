a7cad831082603badc4ea4d73bacec79ed7838a6
Mitchell <mjf153@uclive.ac.nz>
2020-09-18 21:45:16 +1200

    Database Tests

---
+++ src/test/java/seng202/group10/model/DatabaseConnectionTest.java
@@ -0,0 +1,60 @@
+package seng202.group10.model;
+
+import org.junit.*;
+import org.junit.jupiter.api.Disabled;
+
+import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DatabaseConnectionTest {
+
+    static DatabaseConnection database;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        database = DatabaseConnection.getInstance();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        database.disconnect();
+        File file = new File("database.db");
+        file.delete();
+    }
+
+    @Test
+    public void databaseFileCreationTest() {
+        File file = new File("database.db");
+        assertTrue(file.exists());
+    }
+
+    @Test
+    public void databaseSchemaCreationTest() throws SQLException {
+        ArrayList<String> expected = new ArrayList<String>();
+        expected.add("CREATE TABLE airlines (id integer PRIMARY KEY AUTOINCREMENT,name varchar,alias varchar,iata char(3),icao char(4) UNIQUE,callsign varchar,country varchar)");
+        expected.add("CREATE TABLE airports (id integer PRIMARY KEY AUTOINCREMENT,name varchar,city varchar,country varchar,iata char(3) UNIQUE,icao char(4) UNIQUE,latitude double,longitude double,altitude float,timezone float,dstType char(1),tzDatabase varchar)");
+        expected.add("CREATE TABLE aircrafts (id integer PRIMARY KEY AUTOINCREMENT,name varchar,iata char(3),icao char(4),range double)");
+        expected.add("CREATE TABLE routes (id integer PRIMARY KEY AUTOINCREMENT,airlineCode varchar,sourceAirportCode varchar,destinationAirportCode varchar,stops int)");
+        expected.add("CREATE TRIGGER airlineTriggerIata    AFTER INSERT ON airlines    WHEN (NEW.iata = '' OR NEW.iata = '\\N')BEGIN    UPDATE airlines SET iata = null WHERE id = NEW.id;END");
+        expected.add("CREATE TRIGGER airlineTriggerIcao    AFTER INSERT ON airlines    WHEN (NEW.icao = '' OR NEW.icao = '\\N')BEGIN    UPDATE airlines SET icao = null WHERE id = NEW.id;END");
+        expected.add("CREATE TRIGGER airportTriggerIata    AFTER INSERT ON airports    WHEN (NEW.iata = '' OR NEW.iata = '\\N')BEGIN    UPDATE airports SET iata = null WHERE id = NEW.id;END");
+        expected.add("CREATE TRIGGER airportTriggerIcao    AFTER INSERT ON airports    WHEN (NEW.icao = '' OR NEW.icao = '\\N')BEGIN    UPDATE airports SET icao = null WHERE id = NEW.id;END");
+
+        String sqlQuery = "SELECT sql\n" +
+                "FROM sqlite_master\n" +
+                "WHERE name in ('aircrafts', 'airlines', 'airports', 'routes', 'airlineTriggerIata', 'airlineTriggerIcao', 'airportTriggerIata', 'airportTriggerIcao')";
+
+        ResultSet rs = database.executeQuery(sqlQuery);
+        ArrayList<String> actual = new ArrayList<String>();
+
+        while (rs.next()) {
+            actual.add(rs.getString("sql"));
+        }
+
+        assertEquals(expected, actual);
+    }
+}
