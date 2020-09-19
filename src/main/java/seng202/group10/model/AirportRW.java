a475a1defabac43e429d12f4ec523f545cfc945c
Tom Rizzi <tcr59@uclive.ac.nz>
2020-09-19 18:33:43 +1200

    Patched import airports invalid file.

--- src/main/java/seng202/group10/model/AirportRW.java
+++ src/main/java/seng202/group10/model/AirportRW.java
@@ -92,7 +92,7 @@ public class AirportRW extends RWStream {
             }
 
             return airports;
-        } catch (Exception e) {
+        } catch (IOException | IllegalStateException e) {
             throw new IncompatibleFileException();
         } finally {
             // Close reader
