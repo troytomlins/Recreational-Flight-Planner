3dd0a0f901da01fc463b277ae4aa6842037d3050
joh29 <joh29@uclive.ac.nz>
2020-09-30 12:00:24 +1300

    Aircraft toString

--- src/main/java/seng202/group10/model/Aircraft.java
+++ src/main/java/seng202/group10/model/Aircraft.java
@@ -56,4 +56,8 @@ public class Aircraft {
                 icao.equals(other.getIcao()) &&
                 range == (other.getRange());
     }
+
+    public String toString() {
+        return name;
+    }
 }
